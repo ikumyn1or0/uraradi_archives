7366442a3dc391bfa695ec12ae6a89137add7539
ikumyn1or0 <19ming3@gmail.com>
2022-12-14 17:58:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("裏ラジ アーカイブス🦉")
 
-link1 = "このページは[VTuber大浦るかこ](https://www.youtube.com/@Rukako_Oura)さんが毎週金曜日25時から放送している[裏ラジオウルナイト](https://youtube.com/playlist?list=PLShwbdwZFm3r77Bwrr1quz2CpqJc6BZVL)の放送を[書き起こしAI:Whisper](https://whisper.ai/)によってテキスト化されたデータを記載したものです。"
+link1 = "このページは[VTuber大浦るかこ](https://www.youtube.com/@Rukako_Oura)さんが毎週金曜日25時から放送している[裏ラジオウルナイト](https://youtube.com/playlist?list=PLShwbdwZFm3r77Bwrr1quz2CpqJc6BZVL)を[書き起こしAI:Whisper](https://openai.com/blog/whisper/)が書き起こしたデータをまとめたものです。"
 st.markdown(link1, unsafe_allow_html=True)
 st.write("過去回のテキスト検索・放送日時や放送時間の可視化だけでなく、現在の書き起こしAI技術のキャッチアップや普及のために活用できればと思っています。")
 st.write("まだ１回分の書き起こしデータしか記載できていないですが、現在のんびりと機能追加中です！")
